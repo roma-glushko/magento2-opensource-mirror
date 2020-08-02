abe7ca8e562e805e1a2014ee03366a9442c79aa1
Roman Glushko <r.glushko@atwix.com>
2020-08-02 18:22:20 +0300

    Magento 2.3.5-p1 Open Source

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit0a9a4c2b6e37959eb979485fbbade064::getLoader();
+return ComposerAutoloaderInit4e78f2c40519208992e39240b68ed863::getLoader();
